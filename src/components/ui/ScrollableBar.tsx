2bc1ceb90f96b579b363f7734c22d08a04b72d75
daseul-jang <jds940823@naver.com>
2023-08-07 23:02:22 +0900

    feat: 게시글 목록 구현

--- src/components/ui/ScrollableBar.tsx
+++ src/components/ui/ScrollableBar.tsx
@@ -3,13 +3,12 @@ import 'react-multi-carousel/lib/styles.css';
 
 const responsive = {
   desk: {
-    // the naming can be any, depends on you.
     breakpoint: { max: 4000, min: 576 },
     items: 6,
   },
   mobile: {
     breakpoint: { max: 576, min: 0 },
-    items: 5,
+    items: 3,
   },
 };
 
@@ -18,7 +17,6 @@ export default function ScrollableBar({
 }: {
   children: React.ReactNode;
 }) {
-  //console.log(children);
   return (
     <Carousel containerClass='w-full flex gap-2' responsive={responsive}>
       {children}
