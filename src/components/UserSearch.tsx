7b911430c434f6071e05aaa95a007c6e959ca325
daseul-jang <jds940823@naver.com>
2023-08-14 01:25:07 +0900

    rename: hooks 폴더 추가 및 hooks.ts -> debounce.ts 로 이름 변경 & 폴더 이동

--- src/components/UserSearch.tsx
+++ src/components/UserSearch.tsx
@@ -5,7 +5,7 @@ import { useState } from 'react';
 import useSWR from 'swr';
 import GridSpinner from './ui/GridSpinner';
 import UserCard from './UserCard';
-import { useDebounce } from '@/util/hooks';
+import { useDebounce } from '@/hooks/debounce';
 
 export default function UserSearch() {
   const [keyword, setKeyword] = useState('');
